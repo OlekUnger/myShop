86323ac8c138c5492495f4ec919417b8cc1bc497
olekunger <ounger@inbox.ru>
2019-02-28 17:52:44 +0300

    errors

--- public/index.php
+++ public/index.php
@@ -3,4 +3,5 @@ require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
 
 new \myshop\App();
-debug(\myshop\App::$app->getProperties());
+
+throw new Exception('Страница не найдена', 500);
